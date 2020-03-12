2363972feae0e7648f9810e89142f54ad0b7478c
Administrator <xuzey00@gmail.com>
2020-03-11 21:08:28 -0700

    rough attempts

--- src/MaxHeap.java
+++ src/MaxHeap.java
@@ -1,3 +1,7 @@
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.PriorityQueue;
+
 public class MaxHeap implements Heap {
     int size;
 
@@ -12,17 +16,31 @@ public class MaxHeap implements Heap {
     // to be implemented in O(nlogn)
     public void MaxHeapLogN(Integer[] data) {
         // homework
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Arrays.asList(data));
+        maxHeap.addAll(Arrays.asList(data));
+
     }
 
     // build a heap based on data
     // to be implemented in O(n)
     public void MaxHeapN(Integer[] data) {
         // homework
+
     }
 
     // add an item to the heap
     public boolean add(Integer item) {
         // homework
+        if(size != data.length){
+            for(int i = 0; i<data.length;i++){
+                if(data[i] == null){
+                    data[i] = item;
+                    size++;
+                    return true;
+                }
+            }
+        }
+        System.out.println("max capacity reached");
         return false;
     }
 
@@ -36,5 +54,17 @@ public class MaxHeap implements Heap {
     public Integer pop() {
         // homework
         return null;
+    }
+
+    public static void main(String[] args) {
+        int testSize = 5;
+        Integer[] data = new Integer[testSize];
+        MaxHeap heap = new MaxHeap(testSize);
+        for(int i = 0; i<data.length;i++){
+            data[i]=i;
+        }
+        System.out.println(Arrays.toString(data));
+        //System.out.println(heap.MaxHeapLogN(data)); to check the result
+        heap.MaxHeapLogN(data);
     }
 }
