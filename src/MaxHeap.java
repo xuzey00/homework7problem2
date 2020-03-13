d576fe2ffe23c7770e845ba28b952ccf4af383c0
Administrator <xuzey00@gmail.com>
2020-03-13 16:41:25 -0700

    close to the final solution

--- src/MaxHeap.java
+++ src/MaxHeap.java
@@ -1,7 +1,4 @@
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.NoSuchElementException;
-import java.util.PriorityQueue;
+import java.util.*;
 import java.lang.Math;
 
 public class MaxHeap implements Heap {
@@ -16,50 +13,73 @@ public class MaxHeap implements Heap {
 
     // build a heap based on data
     // to be implemented in O(nlogn)
-    public void MaxHeapLogN(Integer[] data) {
+    public void MaxHeapLogN(Integer[] inputArray) {
         // homework
-        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Arrays.asList(data));
-        maxHeap.addAll(Arrays.asList(data));
-
+        if(inputArray.length == 0){
+            return;
+        }
+        for (Integer datum : inputArray) {
+            add(datum);
+        }
     }
 
     // build a heap based on data
     // to be implemented in O(n)
-    public void MaxHeapN(Integer[] data) {
+    public void MaxHeapN(Integer[] inputArray) {
         // homework
+
+        for(int i = (int)Math.floor( inputArray.length - 1 ); i >= 0; i--){
+            heapifyDown(inputArray, i);
+        }
 
     }
 
-    public int rightChild(int pos){
+    private void maxHeapify(Integer[] inputArray, int n){
+        int largest = n;
+        int right = rightChild(n);
+        int left = leftChild(n);
+        if(left < inputArray.length && inputArray[left] > inputArray[largest]){
+            largest = left;
+        }
+        if(right < inputArray.length && inputArray[right] > inputArray[largest]){
+            largest = right;
+        }
+        if(largest != n){
+            swap(n,largest);
+            maxHeapify(inputArray, largest);
+        }
+    }
+
+    private int rightChild(int pos){
+        return (2*pos) + 2;
+    }
+
+    private int leftChild(int pos){
         return (2*pos) + 1;
     }
 
-    public int leftChild(int pos){
-        return (2*pos);
-    }
-
-    public int parent(int pos){
+    private int parent(int pos){
         return pos/2;
     }
 
-    public void swap(int f, int s){
+    private void swap(int f, int s){
         int temp = data[f];
         data[f] = data[s];
         data[s] = temp;
     }
 
-    public boolean isLeaf(int pos){
+    private boolean isLeaf(int pos){
         return pos >= (size / 2) && pos <= size;
     }
 
-    public void heapifyUp(int n){
-        if(n != 1 && data[n] < data[parent(n)]){
+    private void heapifyUp(int n){
+        if(n != 1 && data[n] > data[parent(n)]){
             swap(n,parent(n));
             heapifyUp(parent(n));
         }
     }
 
-    public void heapifyDown(Integer[] arr, int n){
+    private void heapifyDown(Integer[] arr, int n){
         int smallest = n;
         int l = leftChild(n);
         int r = rightChild(n);
@@ -90,35 +110,53 @@ public class MaxHeap implements Heap {
     // return the max item in the heap
     public Integer get() {
         // homework
+        if(size != 0){
+            return data[0];
+        }
         return null;
     }
 
     // remove the root item
     public Integer pop() {
         // homework
-        return null;
+        if(size == 0){
+            return null;
+        }
+        int key = data[0];
+        data[0] = data[size-1];
+        size--;
+        heapifyDown(data, 0);
+        return key;
     }
 
     public void print()
     {
-        for (int i = 1; i <= size / 2; i++ )
-        {
-            System.out.print(" PARENT : " + data[i] + " LEFT CHILD : " + data[2*i]
-                    + " RIGHT CHILD :" + data[2 * i  + 1]);
-            System.out.println();
+        System.out.println("Heap =");
+        for (int i = 0; i<data.length; i++ ) {
+            System.out.print(data[i]+" ");
         }
     }
 
+    //for test
     public static void main(String[] args) {
-        int testSize = 5;
+        int testSize = 8;
         Integer[] data = new Integer[testSize];
         MaxHeap heap = new MaxHeap(testSize);
-        for(int i = 0; i<data.length;i++){
+        Random rand = new Random();
+        for(int i = 0; i< data.length;i++){
             int rm = (int) (Math.random()*((10-1)+1))+1;
-            data[i]= rm;
+            //int rm = (rand.nextInt(1000)%10);
+            data[i] = rm;
         }
-        System.out.println(Arrays.toString(data));
-        heap.MaxHeapN(data);
+        //System.out.println(Arrays.toString(data));
+        heap.MaxHeapLogN(data);
         heap.print();
+        int get = heap.get();
+        System.out.println("\n\nget(): "+get+"\n");
+        heap.pop();
+        System.out.println("after pop():");
+        heap.print();
+        //heap.MaxHeapN(data);
+        //heap.print();
     }
 }
