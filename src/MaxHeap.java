976913ef477080581760c5a309bf0adeaeef4e42
Administrator <xuzey00@gmail.com>
2020-03-13 10:47:11 -0700

    change few things

--- src/MaxHeap.java
+++ src/MaxHeap.java
@@ -1,5 +1,6 @@
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.NoSuchElementException;
 import java.util.PriorityQueue;
 import java.lang.Math;
 
@@ -29,6 +30,14 @@ public class MaxHeap implements Heap {
 
     }
 
+    public int rightChild(int pos){
+        return (2*pos) + 1;
+    }
+
+    public int leftChild(int pos){
+        return (2*pos);
+    }
+
     public int parent(int pos){
         return pos/2;
     }
@@ -39,27 +48,43 @@ public class MaxHeap implements Heap {
         data[s] = temp;
     }
 
+    public boolean isLeaf(int pos){
+        return pos >= (size / 2) && pos <= size;
+    }
+
     public void heapifyUp(int n){
         if(n != 1 && data[n] < data[parent(n)]){
-            swap(n, parent(n));
+            swap(n,parent(n));
             heapifyUp(parent(n));
         }
     }
 
+    public void heapifyDown(Integer[] arr, int n){
+        int smallest = n;
+        int l = leftChild(n);
+        int r = rightChild(n);
+        if(l <= n && data[l] < data[smallest]){
+            smallest = l;
+        }
+        if(r <= n && data[r] < data[smallest]){
+            smallest = r;
+        }
+        if(smallest != n){
+            swap(smallest,n);
+            heapifyDown(data, smallest);
+        }
+    }
+
+
     // add an item to the heap
     public boolean add(Integer item) {
         // homework
-        if(size != data.length){
-            for(int i = 0; i<data.length;i++){
-                if(data[i] == null){
-                    data[i] = item;
-                    size++;
-                    return true;
-                }
-            }
+        if(size == data.length){
+            return false;
         }
-        System.out.println("max capacity reached");
-        return false;
+        data[size++] = item;
+        heapifyUp(size-1);
+        return true;
     }
 
     // return the max item in the heap
@@ -74,6 +99,16 @@ public class MaxHeap implements Heap {
         return null;
     }
 
+    public void print()
+    {
+        for (int i = 1; i <= size / 2; i++ )
+        {
+            System.out.print(" PARENT : " + data[i] + " LEFT CHILD : " + data[2*i]
+                    + " RIGHT CHILD :" + data[2 * i  + 1]);
+            System.out.println();
+        }
+    }
+
     public static void main(String[] args) {
         int testSize = 5;
         Integer[] data = new Integer[testSize];
@@ -83,7 +118,7 @@ public class MaxHeap implements Heap {
             data[i]= rm;
         }
         System.out.println(Arrays.toString(data));
-        //System.out.println(heap.MaxHeapLogN(data)); to check the result
-        heap.MaxHeapLogN(data);
+        heap.MaxHeapN(data);
+        heap.print();
     }
 }
